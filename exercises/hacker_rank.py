fa89641236b09337a5623875131686a195222661
Igor Montrezor <igor.reis.mtrz@gmail.com>
2026-03-16 17:24:43 -0300

    Atualization of exercises hacker rank

--- exercises/hacker_rank.py
+++ exercises/hacker_rank.py
@@ -26,3 +26,14 @@ result = arrays(arr)
 print(result.reshape(3, 3))'''
 
 
+
+import numpy
+
+n,m = map(int,input().split(' '))
+arr = []
+for _ in range(n):
+    row = list(map(int,input().split()))
+    arr.append(row)
+print(numpy.transpose(arr))
+arr_flat = numpy.array(arr)
+print(arr_flat.flatten())
